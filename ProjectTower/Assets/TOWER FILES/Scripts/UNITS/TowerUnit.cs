a149aeb0ccbcfa9b0a459b901294f41855ca1c38
Ivan <ivansaul.catalan@benilde.edu.ph>
2025-06-07 19:54:10 +0800

    Added simple archer tower

--- ProjectTower/Assets/TOWER FILES/Scripts/UNITS/TowerUnit.cs
+++ ProjectTower/Assets/TOWER FILES/Scripts/UNITS/TowerUnit.cs
@@ -41,7 +41,10 @@ public class TowerUnit : MonoBehaviour
 
     public virtual void Attack()
     {
-        if (canAttack) {
+        if (canAttack)
+        {
+        
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
