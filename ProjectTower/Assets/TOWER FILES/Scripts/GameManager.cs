3584eed2e7ee9d58950153e672ec866e6c1c517c
Gerald <geraldperry.reyes@benilde.edu.ph>
2025-06-17 22:33:52 +0800

    edited game manager script

--- ProjectTower/Assets/TOWER FILES/Scripts/GameManager.cs
+++ ProjectTower/Assets/TOWER FILES/Scripts/GameManager.cs
@@ -1,9 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [Header("REFERENCES")]
+    //[Header("REFERENCES")]
     [SerializeField] private GameObject summonPanel;
+    [SerializeField] private bool isGameOver;
+
+
+    [SerializeField] private int cardOnHand;
+
+    private void Start()
+    {
+        isGameOver = false;
+        
+        
+    }
+
+    private void Update()
+    {
+        
+    }
 }
