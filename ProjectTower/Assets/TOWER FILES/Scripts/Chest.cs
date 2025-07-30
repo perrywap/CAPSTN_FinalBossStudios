7db136c581ec59f5fe04ead0c9f54a71ae7fb9d7
Gerald <geraldperry.reyes@benilde.edu.ph>
2025-07-31 02:34:17 +0800

    Polished and added chest animations

--- ProjectTower/Assets/TOWER FILES/Scripts/Chest.cs
+++ ProjectTower/Assets/TOWER FILES/Scripts/Chest.cs
@@ -10,17 +10,35 @@ public class Chest : MonoBehaviour
 
     private void Start()
     {
+
+    }
+
+    public void OpenChest()
+    {
+
+        Debug.Log("SHould open chest");
+        this.GetComponent<Animator>().SetTrigger("Open");
+    }
+
+    public void SpawnCard()
+    {
         rewardCard = PersistentData.Instance.upgradeCards[Random.Range(0, PersistentData.Instance.upgradeCards.Count)];
         UpgradeCard upgrade = rewardCard.GetComponent<UpgradeCard>();
         upgrade.cardType = CardType.CHEST;
         upgrade.isClickable = false;
 
-        GameObject rewardGO = Instantiate(rewardCard);
-        rewardGO.transform.SetParent(chestParent);              
+        GameObject rewardGO = Instantiate(rewardCard, chestParent);
+        rewardGO.transform.SetParent(chestParent);
 
-        for (int i = 0; i <  PersistentData.Instance.unitDatas.Count; i++)
+        for (int i = 0; i < PersistentData.Instance.unitDatas.Count; i++)
         {
             upgrade.Activate(i);
-        } 
+        }
+    }
+
+    public IEnumerator OnRewardReceived()
+    {
+        yield return new WaitForSecondsRealtime(2f);
+        Destroy(this.gameObject);
     }
 }
