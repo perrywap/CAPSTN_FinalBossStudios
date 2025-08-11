1b23f80f75f5ce303bccd9ff739458a670d52a89
Ivan <ivansaul.catalan@benilde.edu.ph>
2025-08-11 23:21:41 +0800

    Adjust mana regen rate // mortar attack rate

--- ProjectTower/Assets/TOWER FILES/Scripts/PersistentData.cs
+++ ProjectTower/Assets/TOWER FILES/Scripts/PersistentData.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
